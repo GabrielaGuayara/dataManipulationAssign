4511db7d267648599a121bd5efb21e7a38b1df85
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-09 09:26:53 -0400

    Solved exercise part one

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // The initial numbers that must be verified.
-const n1 = 16;
+const n1 = 20;
 const n2 = 5;
-const n3 = 6;
-const n4 = 35;
+const n3 = 10;
+const n4 = 15;
 
 // Check one: add up to 50
 const isSum50 = (n1 + n2 + n3 + n4) == 50;
@@ -20,4 +20,23 @@ const isUnique = n1 != n2 && n1 != n3 && n1 != n4 && n2 != n3 && n2 != n4 && n3 
 const isValid = isSum50 && isTwoOdd && !isOver25 && isUnique;
 
 // Finally, log the results.
-console.log(isValid);
+onsole.log(`The four number are valid according to the provided criteria is ${isValid}`)
+
+
+//PART 1: MATH PROBLEMS
+
+//Check five: all numbers are divisible by 5
+const isDivByFive = ((n1%5)==0)&&((n2%5)==0)&&((n3%5)==0)&&((n4%5)==0);
+
+//Check if the first number is greater than the last one
+const isGreaterThanTheFirt = n1>n4;
+
+//Aritmetic Chain
+let operation =(((n1-n2)*n3)%n4)
+
+//Check is the numbers are over 25
+const isOver25_otherMethod = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
+
+const isValidPartOne = isDivByFive && isGreaterThanTheFirt && isOver25_otherMethod;
+
+console.log(`The four number are valid according to the provided criteria is ${isValidPartOne}`)
