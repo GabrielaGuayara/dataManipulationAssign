4cfe88ef7602d394c8d76e68ca00182bb83aa377
Gabriela Guayara <gabriela.guayara@gmail.com>
2024-05-09 21:11:44 -0400

    Declared variables for part two

--- app.js
+++ app.js
@@ -40,3 +40,28 @@ const isOver25_otherMethod = n1 < 25 && n2 < 25 && n3 < 25 && n4 < 25;
 const isValidPartOne = isDivByFive && isGreaterThanTheFirt && isOver25_otherMethod;
 
 console.log(`The four number are valid according to the provided criteria is ${isValidPartOne}`)
+
+//PART TWO: Pratical Math
+/**
+ * You are planning a cross-country road trip!
+The distance of the trip, in total, is 1,500 miles.
+Your car’s fuel efficiency is as follows:
+At 55 miles per hour, you get 30 miles per gallon.
+At 60 miles per hour, you get 28 miles per gallon.
+At 75 miles per hour, you get 23 miles per gallon.
+You have a fuel budget of $175.
+The average cost of fuel is $3 per gallon.
+Set up a program to answer the following questions:
+How many gallons of fuel will you need for the entire trip?
+Will your budget be enough to cover the fuel expense?
+How long will the trip take, in hours?
+Compare the results when traveling at an average of 55, 60, and 75 miles per hour. Which makes the most sense for the trip? 
+ */
+
+const distance = 1500;
+const budget = 175;
+let cost = 3.00;
+
+
+
+
